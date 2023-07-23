4cea390f15f197137c0ff97f402a682c3e15da31
Durgesh Kuchi <kuchid8144@uhcl.edu>
2023-07-23 19:29:40 -0400

    branch2 1'

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const server = http.createServer((req,res)=>{
     res.write("Home")
     console.log("Successfully Created server")
 })
+http.get('/',)
 server.listen(3000,()=>{
     console.log('srever created at port 3000..')
 })
